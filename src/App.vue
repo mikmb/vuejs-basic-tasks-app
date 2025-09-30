459a0f9b1fdf5388eeb5905a03041153c855e1e5
mikmb <1057416+mikmb@users.noreply.github.com>
2025-09-30 14:27:32 -0700

    basic Vue.js tasks project

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+
+
+<!-- Using Composition API - shorter version-->
+<script setup>
+import { ref, onMounted } from 'vue';
+
+      const name = ref('John Doe');
+      const status = ref('active');
+      const tasks = ref(['Task 1', 'Task 2', 'Task 3']);
+      const newTask = ref('');
+
+      const toggleStatus = () => {
+        if(status.value === 'active') {
+        status.value = 'pending';
+      } else if(status.value === 'pending') {
+        status.value = 'inactive';
+      } else {
+        status.value = 'active';
+      }
+    }
+
+    const addTask = () => {
+      if(newTask.value.trim() !== '') {
+        tasks.value.push(newTask.value);
+      }
+      newTask.value = '';
+    }
+
+    const deleteTask = (index) => {
+      tasks.value.splice(index, 1);
+    }
+
+    onMounted(async() => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const data = await response.json();
+        tasks.value = data.map(task => task.title);
+      } catch (error) {
+        console.error('Error fetching tasks:', error);
+      }
+    
+    });
+  
+</script>
+
+<template>
+  <h1>{{name}}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <!-- By adding .prevent we don't have to do e.preventDefault() in the addTask function -->
+  <form @submit.prevent="addTask">
+    <label for=""newTask>Add Task</label>
+    <!-- Bind data to input using v-model directive -->
+    <input type="text" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Submit</button>
+  </form>
+
+  <br />
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="(task, index) in tasks" :key="task"><span>{{task}}</span> <button @click="deleteTask(index)">Delete</button></li>
+  </ul>
+
+  <br />
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
+</template>
---
+++ src/AppCompositionAPI_longer_version.vue
@@ -0,0 +1,45 @@
+
+
+<!-- Using Composition API - longer version -->
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+      const name = ref('John Doe');
+      const status = ref('active');
+      const tasks = ref(['Task 1', 'Task 2', 'Task 3']);
+
+      const toggleStatus = () => {
+        if(status.value === 'active') {
+        status.value = 'pending';
+      } else if(status.value === 'pending') {
+        status.value = 'inactive';
+      } else {
+        status.value = 'active';
+      }
+    }
+
+    return {
+      name,
+      status,
+      tasks,
+      toggleStatus
+    };
+  }
+}
+</script>
+<template>
+  <h1>{{name}}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{task}}</li>
+  </ul>
+
+  <br />
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
+</template>
